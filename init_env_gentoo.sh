f5347167522bacb65add606bcd8fe4bdaacbf95b
Raphaël Vinot <raphael@vinot.info>
2011-06-25 23:02:50 -0400

    Force add submodules

--- init_env_gentoo.sh
+++ init_env_gentoo.sh
@@ -8,11 +8,11 @@ sudo emerge -va dev-python/rlcompleter2 app-vim/nerdtree app-vim/fugitive \
 
 rm -rf _vim/bundle/*
 
-git submodule add https://github.com/tpope/vim-git.git _vim/bundle/git
-git submodule add https://github.com/fs111/pydoc.vim.git _vim/bundle/pydoc
-git submodule add https://github.com/reinh/vim-makegreen _vim/bundle/makegreen
-git submodule add https://github.com/vim-scripts/TaskList.vim.git _vim/bundle/tasklist
-git submodule add https://github.com/sontek/rope-vim.git _vim/bundle/ropevim
-git submodule add https://github.com/vim-scripts/AutoComplPop.git _vim/bundle/acp
-git submodule add https://github.com/altercation/vim-colors-solarized.git _vim/bundle/solarized
+git submodule add -f https://github.com/tpope/vim-git.git _vim/bundle/git
+git submodule add -f https://github.com/fs111/pydoc.vim.git _vim/bundle/pydoc
+git submodule add -f https://github.com/reinh/vim-makegreen _vim/bundle/makegreen
+git submodule add -f https://github.com/vim-scripts/TaskList.vim.git _vim/bundle/tasklist
+git submodule add -f https://github.com/sontek/rope-vim.git _vim/bundle/ropevim
+git submodule add -f https://github.com/vim-scripts/AutoComplPop.git _vim/bundle/acp
+git submodule add -f https://github.com/altercation/vim-colors-solarized.git _vim/bundle/solarized
 
