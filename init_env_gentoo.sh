156fda32c5054efb1280f79265cbdc02d598489f
Raphaël Vinot <raphael@vinot.info>
2011-10-31 22:17:06 -0400

    update install script

--- init_env_gentoo.sh
+++ init_env_gentoo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Global stuff
-sudo emerge -va screen tmux vim genlop g-cpan
+sudo emerge -vaDN screen tmux vim genlop g-cpan
 
 # used by dotfiles
 sudo emerge -vaDN dev-python/rlcompleter2 app-vim/nerdtree app-vim/fugitive \
--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 OS="gentoo"
 
 function link_file {
