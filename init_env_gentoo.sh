eee501c227713a142b63ac303ef3baf67a90bda8
Raphaël Vinot <raphael@vinot.info>
2011-11-19 00:00:46 -0500

    enable bashcomp scripts

--- init_env_gentoo.sh
+++ init_env_gentoo.sh
@@ -22,3 +22,48 @@ git submodule add https://github.com/sontek/rope-vim.git _vim/bundle/ropevim
 git submodule add https://github.com/vim-scripts/AutoComplPop.git _vim/bundle/acp
 git submodule add https://github.com/altercation/vim-colors-solarized.git _vim/bundle/solarized
 
+
+eselect bashcomp enable base
+eselect bashcomp enable bash-builtins
+eselect bashcomp enable bind-utils
+eselect bashcomp enable brctl
+eselect bashcomp enable bzip2
+eselect bashcomp enable configure
+eselect bashcomp enable coreutils
+eselect bashcomp enable crontab
+eselect bashcomp enable e2fsprogs
+eselect bashcomp enable eix
+eselect bashcomp enable eselect
+eselect bashcomp enable gcc
+eselect bashcomp enable genlop
+eselect bashcomp enable gentoo
+eselect bashcomp enable git
+eselect bashcomp enable gzip
+eselect bashcomp enable iftop
+eselect bashcomp enable ifupdown
+eselect bashcomp enable iproute2
+eselect bashcomp enable ipsec
+eselect bashcomp enable iptables
+eselect bashcomp enable layman
+eselect bashcomp enable lsof
+eselect bashcomp enable lvm
+eselect bashcomp enable lzma
+eselect bashcomp enable make
+eselect bashcomp enable man
+eselect bashcomp enable mdadm
+eselect bashcomp enable module-init-tools
+eselect bashcomp enable mount
+eselect bashcomp enable nmap
+eselect bashcomp enable openssl
+eselect bashcomp enable rsync
+eselect bashcomp enable screen
+eselect bashcomp enable sh
+eselect bashcomp enable shadow
+eselect bashcomp enable ssh
+eselect bashcomp enable strace
+eselect bashcomp enable tar
+eselect bashcomp enable tcpdump
+eselect bashcomp enable util-linux
+eselect bashcomp enable vim
+eselect bashcomp enable xrandr
+
