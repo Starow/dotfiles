3e809d172c0ed18c5e350a975fbbd578f92a6c64
Raphaël Vinot <raphael@vinot.info>
2014-04-20 14:14:34 +0200

    update for ubuntu 14.04

--- init_env_ubuntu.sh
+++ init_env_ubuntu.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 sudo apt-get install vim vim-scripts vim-nox pylint build-essential \
-                     exuberant-ctags tmux ttf-inconsolata htop
+                     exuberant-ctags tmux fonts-inconsolata htop
 
-pip install flake8
+sudo pip install --upgrade flake8
 
 git rm -r --cached _vim/bundle/
 rm -rf _vim/bundle/*
 
-git submodule add http://github.com/tpope/vim-fugitive.git _vim/bundle/fugitive
-git submodule add https://github.com/tpope/vim-git.git _vim/bundle/git
-git submodule add https://github.com/altercation/vim-colors-solarized.git _vim/bundle/solarized
-git submodule add https://github.com/xolox/vim-easytags.git _vim/bundle/easytags
-git submodule add https://github.com/scrooloose/syntastic.git _vim/bundle/syntastic
-git submodule add https://github.com/xolox/vim-misc _vim/bundle/misc
+git submodule add --force http://github.com/tpope/vim-fugitive.git _vim/bundle/fugitive
+git submodule add --force https://github.com/tpope/vim-git.git _vim/bundle/git
+git submodule add --force https://github.com/altercation/vim-colors-solarized.git _vim/bundle/solarized
+git submodule add --force https://github.com/xolox/vim-easytags.git _vim/bundle/easytags
+git submodule add --force https://github.com/scrooloose/syntastic.git _vim/bundle/syntastic
+git submodule add --force https://github.com/xolox/vim-misc _vim/bundle/misc
 
 
 cp /usr/share/vim-scripts/colors/desert256.vim _vim/colors/
